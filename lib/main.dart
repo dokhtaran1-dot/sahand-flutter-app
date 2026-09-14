2562ab48d59b11a3a6596e944cc27a62b9090952
Hamed <dokhtaran1@gmail.com>
2026-09-15 03:05:01 +0330

    Restore approved Royal1 entry screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'welcome_page.dart';
 import 'src/home_page.dart';
 
 void main() {
-  runApp(const RoyalOneApp());
+  runApp(const EntryApp());
 }
 
-class RoyalOneApp extends StatelessWidget {
-  const RoyalOneApp({super.key});
+class EntryApp extends StatefulWidget {
+  const EntryApp({super.key});
+
+  @override
+  State<EntryApp> createState() => _EntryAppState();
+}
+
+class _EntryAppState extends State<EntryApp> {
+  bool _entered = false;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    if (_entered) {
+      return const MaterialApp(
+        title: 'ROYAL 1',
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      );
+    }
+
+    return MaterialApp(
       title: 'ROYAL 1',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: WelcomePage(
+        onEnter: () {
+          setState(() {
+            _entered = true;
+          });
+        },
+      ),
     );
   }
 }
