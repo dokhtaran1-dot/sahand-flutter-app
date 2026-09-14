3976b41d450ac76940a2546f9e5378b2657be68e
Hamed <dokhtaran1@gmail.com>
2026-09-15 02:07:43 +0330

    Remove obsolete welcome screen and launch Royal1 home directly

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'welcome_page.dart';
 import 'src/home_page.dart';
 
 void main() {
-  runApp(const EntryApp());
+  runApp(const RoyalOneApp());
 }
 
-class EntryApp extends StatefulWidget {
-  const EntryApp({super.key});
-
-  @override
-  State<EntryApp> createState() => _EntryAppState();
-}
-
-class _EntryAppState extends State<EntryApp> {
-  bool _entered = false;
+class RoyalOneApp extends StatelessWidget {
+  const RoyalOneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (_entered) {
-      return const MaterialApp(
-        title: 'ROYAL 1',
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      );
-    }
-
-    return MaterialApp(
+    return const MaterialApp(
       title: 'ROYAL 1',
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(
-        onEnter: () {
-          setState(() {
-            _entered = true;
-          });
-        },
-      ),
+      home: HomePage(),
     );
   }
 }
