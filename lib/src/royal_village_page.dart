db635cf177e34facd38440586e614fa9da8f8853
Hamed <dokhtaran1@gmail.com>
2026-09-23 00:08:40 +0330

    Open Royal Village salon gallery directly from Home

--- lib/src/royal_village_page.dart
+++ lib/src/royal_village_page.dart
@@ -1,5 +1,6 @@
-import 'royal_village_entry_page.dart';
+import 'rv_gallery_page.dart';
 
-class RoyalVillagePage extends RoyalVillageEntryPage {
+// The RV tile on Home opens the salon selection as the third app screen.
+class RoyalVillagePage extends RvGalleryPage {
   const RoyalVillagePage({super.key});
 }
